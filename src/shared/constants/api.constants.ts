a0128ad88df2a11cb88403571db8352e1f85708c
Унгер Евгений <en.unger@gmail.com>
2021-03-05 17:15:28 +0500

    feat: add Loader, api and utils

---
+++ src/shared/constants/api.constants.ts
@@ -0,0 +1,9 @@
+export const API_URLS = {
+  BASE_URL: "https://api.openweathermap.org/data/2.5",
+};
+
+export const API_KEY = "c297176777e62aee3b689a54f518a692";
+
+export const DEFAULT_TIMESTAMPS = 1;
+export const TIMESTAMPS_STEP = 1;
+export const MAX_TIMESTAMPS = 40;
